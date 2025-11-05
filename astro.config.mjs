36229580a219e42fa6db4e73f88a20125dc8d06c
Alejandro <alex478y@protonmail.com>
2025-11-05 11:57:51 -0500

    Add initial configuration and asset ignore for Cloudflare Worker
    - Created a new .assetsignore file to exclude _worker.js and _routes.json from asset uploads.
    - Added wrangler.jsonc configuration file to define worker settings, compatibility flags, and asset directory.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+	output: "server",
 
-  integrations: [react()]
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	integrations: [react()],
+	adapter: cloudflare(),
 });

240454e35dab8cf6ab72cddc94cf95ef98bb5c1a
Alejandro <alex478y@protonmail.com>
2025-11-18 16:38:53 -0500

    refactor: rename TourWrapper to AppWrapper and remove tour-related logic

--- src/stores/tourActive.ts
+++
@@ -1,8 +0,0 @@
-import { TOUR_STORAGE_KEY } from "@/utils/types/const";
-import { atom } from "nanostores";
-
-export type statusTour = "true" | "false";
-
-export const tourActiveStore = atom<statusTour>(
-	(localStorage.getItem(TOUR_STORAGE_KEY) as statusTour) || "true"
-);

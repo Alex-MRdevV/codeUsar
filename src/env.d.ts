09353e74ec35828a033234afcd90310609593f4e
Alex <alex478y@protonmail.com>
2025-11-30 10:02:33 -0500

    refactor: Remove 'jose' dependency and clean up related type definitions

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,16 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-import { JWTPayload } from "jose";
 
 declare global {
-	namespace App {
-		interface Locals extends Record<string, any> {
-			user: JWTPayload;
-		}
-		interface Env {
-			DB: D1Database;
-		}
-	}
+	namespace App {}
 }
 
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;

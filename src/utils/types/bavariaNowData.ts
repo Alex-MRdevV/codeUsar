3c6e9b02fa3e49521eda9df1fe318c87481f28f4
Alex <alex478y@protonmail.com>
2025-12-02 13:50:37 -0500

    feat: Remove unused imports from messages.ts for cleaner code

--- src/utils/types/bavariaNowData.ts
+++ src/utils/types/bavariaNowData.ts
@@ -38,3 +38,4 @@ export type ResultadoAgrupado = {
 		errors: string[];
 	}>;
 };
+
--- src/utils/types/messages.ts
+++ src/utils/types/messages.ts
@@ -1,6 +1,3 @@
-import { type ResultadoAgrupado as ResultBavaria } from "@/utils/types/bavariaNowData";
-import { type ResultadoAgrupado } from "@/utils/types/consolidadoData";
-
 export type PersistedConsolidado = {
 	byStatus: {
 		enRuta: ReducedCliente[];
